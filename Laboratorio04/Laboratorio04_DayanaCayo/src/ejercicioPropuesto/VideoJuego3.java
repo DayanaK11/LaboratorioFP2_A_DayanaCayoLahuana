a83b315c15f773c7db4cfe49b727ec016ca4dfe5
DayanaK11 <dayanacayol@gmail.com>
2024-05-16 08:31:55 -0500

    Iniciando con ejercicio propuesto lab 04

---
+++ Laboratorio04/Laboratorio04_DayanaCayo/src/ejercicioPropuesto/VideoJuego3.java
@@ -0,0 +1,30 @@
+package ejercicioPropuesto;
+import java.util.*;
+
+public class VideoJuego3 {
+
+	public static void main(String[] args) {
+		ArrayList<ArrayList<Soldado>> videojuego = new ArrayList<ArrayList<Soldado>>();
+
+	}
+	
+	//entre 1-10 soldados
+	public void generarSoldados(ArrayList<ArrayList<Soldado>> videojuego) {
+		Random rand = new Random();
+		
+	}
+	//entre 1 -5
+	public void generarVida() {
+		
+	}
+	
+	public void inicializarTablero() {
+		
+	}
+	//fila y columna también autogenerados aleatoriamente
+	public void generarPosicion() {
+		
+	}
+	
+
+}
